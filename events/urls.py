6b37ad8efccaa7cebe296b5def4108530e8fed38
Marazul <marazulislam964@gmail.com>
2025-07-28 22:03:32 +0600

    Update urls.py

--- events/urls.py
+++ events/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from events.views import manager_dashboard, employee_dashboard, create_event, view_event, update_event, delete_event, event_details, dashboard
 
 urlpatterns = [
-    path('manager-dashboard/', manager_dashboard, name="manager-dashboard"),
-    path('user-dashboard/', employee_dashboard, name='user-dashboard'),
-    path('create-event/', create_event, name='create-event'),
-    path('view_event/', view_event),
-    path('event/<int:event_id>/details/', event_details, name='event-details'),
-    path('update-event/<int:id>/', update_event, name='update-event'),
-    path('delete-event/<int:id>/', delete_event, name='delete-event'),
-    path('dashboard', dashboard, name='dashboard')
+    path('manager-dashboard/', manager_dashboard.as_view(), name="manager-dashboard"),
+    path('user-dashboard/', employee_dashboard.as_view(), name='user-dashboard'),
+    path('create-event/', create_event.as_view(), name='create-event'),
+    path('view_event/', view_event.as_view(),name='event-view'),
+    path('event/<int:event_id>/details/', event_details.as_view(), name='event-details'),
+    path('update-event/<int:id>/', update_event.as_view(), name='update-event'),
+    path('delete-event/<int:id>/', delete_event.as_view(), name='delete-event'),
+    path('dashboard', dashboard.as_view(), name='dashboard')
 ]
